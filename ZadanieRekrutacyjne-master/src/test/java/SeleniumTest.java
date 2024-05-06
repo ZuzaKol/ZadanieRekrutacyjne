bfa105655a1ffc99b771d3aa41a0e8a07579fd64
remigiuszmrotek <remigiusz.mrotek-contractor@cognitran.com>
2024-05-06 22:54:31 +0200

    second commit

---
+++ ZadanieRekrutacyjne-master/src/test/java/SeleniumTest.java
@@ -0,0 +1,45 @@
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.*;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
+
+import org.testng.annotations.Test;
+
+public class SeleniumTest {
+
+    @Test
+    public void searchAndEnter5thResult() {
+        //Set Driver path and open google
+        WebDriverManager.chromedriver().setup();
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://www.google.com");
+
+        //Accept cookies
+        driver.findElement(By.cssSelector("button[id='L2AGLb'] div[role='none']")).click();
+
+        //Enter query
+        WebElement p=driver.findElement(By.name("q"));
+        p.sendKeys("Selenium Java");
+        p.sendKeys(Keys.RETURN);
+
+        //Find 5th result
+        String fifthResultXpath = "(//h3[not(ancestor::div[@jsname])])[5]";
+        WebElement fifthResult = driver.findElement(By.xpath(fifthResultXpath));
+
+        //Scroll to element
+        Actions actions = new Actions(driver);
+        actions.moveToElement(fifthResult);
+        actions.perform();
+
+        //Click
+        fifthResult.click();
+
+
+
+
+
+    }
+}
+
+
+
