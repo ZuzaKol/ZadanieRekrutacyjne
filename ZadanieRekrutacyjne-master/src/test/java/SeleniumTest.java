d3f87bba306ed40d453c22fb74d23dcc9e92fe09
ZuzaKol <kolekzuza@gmail.com>
2024-05-07 10:45:46 +0200

    Add validation

--- ZadanieRekrutacyjne-master/src/test/java/SeleniumTest.java
+++ ZadanieRekrutacyjne-master/src/test/java/SeleniumTest.java
@@ -3,7 +3,9 @@ import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.interactions.Actions;
 
+import org.testng.Assert;
 import org.testng.annotations.Test;
+import java.util.List;
 
 public class SeleniumTest {
 
@@ -26,6 +28,14 @@ public class SeleniumTest {
         String fifthResultXpath = "(//h3[not(ancestor::div[@jsname])])[5]";
         WebElement fifthResult = driver.findElement(By.xpath(fifthResultXpath));
 
+
+        //Validation 1
+        String resultsXPath = "(//h3[not(ancestor::div[@jsname])])";
+        List<WebElement> results = driver.findElements(By.xpath(resultsXPath));
+        Assert.assertEquals(results.get(4).getText(), fifthResult.getText(), "validation 1 fail");
+
+
+
         //Scroll to element
         Actions actions = new Actions(driver);
         actions.moveToElement(fifthResult);
@@ -33,6 +43,22 @@ public class SeleniumTest {
 
         //Click
         fifthResult.click();
+
+        //Wait for the page to load
+        try {
+            Thread.sleep(3000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        //Validation 2
+        String actualURL = driver.getCurrentUrl();
+        Assert.assertFalse(actualURL.contains("google"), "Actual URL does contain the word 'google'");
+
+
+
+
+
 
 
     }
