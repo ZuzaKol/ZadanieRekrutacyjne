4b2cc1adb2606b30d949e5839fbdb48fa600cfdf
ZuzaKol <kolekzuza@gmail.com>
2024-05-11 13:54:09 +0200

    New validation

--- ZadanieRekrutacyjne-master/src/test/java/pl/seleniumtest/pages/GooglePage.java
+++ ZadanieRekrutacyjne-master/src/test/java/pl/seleniumtest/pages/GooglePage.java
@@ -7,6 +7,9 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
 
 public class GooglePage {
     protected WebDriver driver;
@@ -16,6 +19,8 @@ public class GooglePage {
     private WebElement searchQuery;
 
     private WebElement result;
+    private String resultTitle;
+
 
     //Initialising PageFactory
     public GooglePage(WebDriver driver) {
@@ -33,23 +38,37 @@ public class GooglePage {
         //Find 5th result
         String resultXpath = String.format("(//h3[not(ancestor::div[@jsname])])[%d]", index);
         result = driver.findElement(By.xpath(resultXpath));
+        //Get title
+        resultTitle = result.getText();
+        if (resultTitle.endsWith(" ...")) {
+            resultTitle = resultTitle.substring(0, resultTitle.length() - 4).trim();
+        }
         //Scroll to element
         Actions actions = new Actions(driver);
         actions.moveToElement(result);
         actions.perform();
-
     }
 
     public void openResult() {
         //Click
         result.click();
-        //Wait for the page to load
-        try {
-            Thread.sleep(3000);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
+        // Wait for the page to load
+        WebDriverWait wait = new WebDriverWait(driver, 10);
+        wait.until(ExpectedConditions.titleContains(resultTitle));
+        //Validation
+        String actualTitle = driver.getTitle();
+        Assert.assertTrue(actualTitle.contains(resultTitle), "Validation fail");
     }
+
+//    public void validation1() {
+//        //Validate presence of desired result
+//        Assert.assertTrue(result.isDisplayed(), "The desired result is not displayed");
+//
+//
+//        String resultsXPath = "(//h3[not(ancestor::div[@jsname])])";
+//        List<WebElement> results = driver.findElements(By.xpath(resultsXPath));
+//        Assert.assertEquals(results.get(4).getText(), fifthResult.getText(), "Validation 1 fail");
+//    }
 }
 
 
